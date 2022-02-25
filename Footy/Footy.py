39a56b552f2f3a0d95c3557ebb9f789d2d925c68
schleising <stephen@schleising.net>
2022-02-25 17:47:41 +0000

    Force timezone to UTC rather than Zulu

--- Footy/Footy.py
+++ Footy/Footy.py
@@ -9,7 +9,7 @@ class Match:
         self.awayTeam = matchData['awayTeam']['name']
         self.homeScore = matchData['score']['fullTime']['homeTeam'] if matchData['score']['fullTime']['homeTeam'] is not None else 'TBD'
         self.awayScore = matchData['score']['fullTime']['awayTeam'] if matchData['score']['fullTime']['awayTeam'] is not None else 'TBD'
-        matchDate = parse(matchData['utcDate'])
+        matchDate = parse(matchData['utcDate'], settings={'TIMEZONE': 'UTC'})
         if matchDate is None:
             self.matchDate = datetime(1900, 1, 1)
         else:
