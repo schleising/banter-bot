0183bda99a38f37d5d2069244fe578ae8fb2ac55
schleising <stephen@schleising.net>
2022-03-20 13:00:07 +0000

    Add some initial banter strings

--- Footy/BantzStrings.py
+++ Footy/BantzStrings.py
@@ -1,43 +1,61 @@
 teamMatchStarted: list[str] = [
-    '{name} match started'
+    "{name} are shit",
+    "{name} smell",
 ]
 
 drawing: list[str] = [
-    '{name} drawing'
+    "{name} level, this is a shit match",
+    "Boring old {name}",
 ]
 
 teamLeadByOne: list[str] = [
-    '{name} lead by one'
+    "{name} cheats, the ref's a cunt",
+    "That was never a goal",
 ]
 
 teamExtendingLead: list[str] = [
-    '{name} extending lead'
+    "There's no way {name} deserve this lead",
+    "Have {name} paid the ref?",
+    "This is bullshit",
+    "The ref's a cunt, you're a cunt",
 ]
 
 teamLosingLead: list[str] = [
-    '{name} losing lead'
+    "{name} are throwing it away",
+    "{name} are rubbish",
+    "{name} fucking it up again",
 ]
 
 teamDeficitOfOne: list[str] = [
-    '{name} deficit of one'
+    "This is more like it",
+    "Oh dear...",
+    "{name} wankers",
 ]
 
 teamExtendingDeficit: list[str] = [
-    '{name} extending deficit'
+    "Starting to feel a bit sorry for {name}",
+    "Never mind, there's always the next game",
+    "Poor {name}",
+    "Whoops...",
 ]
 
 teamLosingDeficit: list[str] = [
-    '{name} losing deficit'
+    "Too little too late for {name}",
+    "{name} won't come back from here",
+    "The ref's a cunt",
 ]
 
 teamWon: list[str] = [
-    '{name} won'
+    "That was a shit game",
+    "There's no way {name} deserved that",
 ]
 
 teamLost: list[str] = [
-    '{name} lost'
+    "Justice done, {name} lost",
+    "Job done for {name}?",
 ]
 
 teamDrew: list [str] = [
-    '{name} drew'
+    "Another draw for {name}",
+    "Boring old {name}",
 ]
