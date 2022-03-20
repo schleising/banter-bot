4edde8f0c60d8daa445852dc3a93161c6de083ef
schleising <stephen@schleising.net>
2022-03-20 13:20:05 +0000

    Add supported strings

---
+++ Footy/SupportedBantzStrings.py
@@ -0,0 +1,62 @@
+teamMatchStarted: list[str] = [
+    "Come on {name} !!",
+    "{name} are the greatest team in the world",
+]
+
+drawing: list[str] = [
+    "{name} winning this match, morally",
+    "{name} should still win this",
+]
+
+teamLeadByOne: list[str] = [
+    "{name} take the lead, deserved, obviously",
+    "That was a fucking great goal !!",
+]
+
+teamExtendingLead: list[str] = [
+    "{name} deserve this lead",
+    "{name} cruising to victory",
+    "This is incredible from {name}",
+    "Easy",
+]
+
+teamLosingLead: list[str] = [
+    "Not worried, {name} should still win",
+    "The ref's been paid off",
+    "The ref's fucking it up again",
+]
+
+teamDeficitOfOne: list[str] = [
+    "Come on {name} !!  We can pull this back",
+    "That was never a goal",
+    "Fucking bullshit !!",
+]
+
+teamExtendingDeficit: list[str] = [
+    "Fuck the patriarchy !!",
+    "More bullshit",
+    "We can still do this",
+]
+
+teamLosingDeficit: list[str] = [
+    "Back in the game",
+    "{name} pulling it back",
+    "Yes !!  Come on {name} !!",
+]
+
+teamWon: list[str] = [
+    "Job done, three points",
+    "Another easy win for {name}",
+    "Justice !!",
+]
+
+teamLost: list[str] = [
+    "That was definitely fixed",
+    "Utter bullshit",
+    "Everyone's so biased against {name}",
+]
+
+teamDrew: list [str] = [
+    "Good result that, we'll take it",
+    "Another point, that'll do",
+]
